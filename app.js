4b3cfac46c61e6cfcbb667b3f4c05d7a040ab70a
ryan <ryanthesmith@gmail.com>
2015-12-30 13:07:38 -0500

    [update] created function to calculate time til specified endTime, appends to div class flipboard

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	$('#end-of-year').countdown();
+
+	// $('#end-of-decade').countdown({
+	// 	endYear: '2019',
+	// });
+
+	// $('#end-of-century').countdown({
+	// 	endYear: '2099',
+	// });
+
+});
--- countdown-plugin.js
+++ countdown-plugin.js
@@ -1,8 +1,6 @@
-(function(?) {
+(function($) {
 
 	$.fn.countdown = function(options) {
-
-		var countdown = this;
 
 		var settings = $.extend({
 			endYear: '2015',
@@ -13,6 +11,7 @@
 
 		var endTime = settings.endMonth 
 			+ ' ' + settings.endDay
+			+ ' ' + settings.endYear
 			+ ' ' + settings.endTime; 
 
 		var timeTilTheEnd = function(endTime) {
@@ -21,15 +20,38 @@
 			var minutes = Math.floor((timeLeft/(1000*60)) % 60);
 			var hours = Math.floor((timeLeft/(1000*60*60)) % 24);
 			var days = Math.floor((timeLeft/(1000*60*60*24)));
+
+			if (days >= 365) {
+			    var years = Math.floor(days/365);
+			    days = Math.floor(days % 365);
+			};
+
 			return {
 				seconds: seconds,
 				minutes: minutes,
 				hours: hours,
 				days: days,
+				years: years ? years: null,
 			};
 		};
 
-		console.log(timeTilEnd(endOfYear));
+		var flipBoard = '<div class="flipboard">'
+			+ '<div class="flip-wrapper">'
+			+ '<div class="flip flip-next"></div>'
+			+ '<div class="flip flip-top"></div>'
+			+ '<div class="flip flip-top flip-back"></div>'
+			+ '<div class="flip flip-bottom"></div>'
+			+ '</div></div>';
+
+		this.append(flipBoard);
+
+		var appendToFlipBoard = function() {
+
+		};
+
+		var timeTilEnd = timeTilTheEnd(endTime);
+		
+
 
 	};
 
